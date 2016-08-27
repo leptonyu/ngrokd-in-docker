e6a262329cfff2e4986df4cc1645e88d6308af43
Daniel Yu <yushaojie@lvjinsuo.com>
2016-08-27 18:20:01 +0800

    modify build client

--- build-client.sh
+++ build-client.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-set -ex \
-&& export DIR="$PWD" \
-&& export NGROK_GO_VERSION=1.7.1 \
-&& pkgroot=github.com/inconshreveable/ngrok \
-&& git clone --branch "${NGROK_GO_VERSION}" https://${pkgroot}.git ngrok \
-&& sed -i.bak "s|go install|go install -a -ldflags '-s'|g" ngrok/Makefile \
-&& sed -i.bak "s|code.google.com\/p\/log4go|github.com/alecthomas/log4go|g" ngrok/src/ngrok/log/logger.go \
-&& cp "$DIR/root.crt" ngrok/assets/client/tls/ngrokroot.crt \
-&& cd ngrok \
-&& CGO_ENABLED=0 make release-client
+export DIR="$PWD"
+export NGROK_GO_VERSION=1.7.1
+
+pkgroot=github.com/inconshreveable/ngrok 
+
+cd "$DIR"
+
+if [ ! -d "ngrok" ]; then
+  git clone --branch "${NGROK_GO_VERSION}" https://${pkgroot}.git ngrok
+  sed -i.bak "s|go install|go install -a -ldflags '-s'|g" ngrok/Makefile
+  sed -i.bak "s|code.google.com\/p\/log4go|github.com/alecthomas/log4go|g" ngrok/src/ngrok/log/logger.go
+fi
+
+ls root.crt.* | while read line; do
+   name=${line##*.}
+   cp "$DIR/$line" ngrok/assets/client/tls/ngrokroot.crt
+   rm -rf "$DIR/bin/$name"
+   mkdir -p "$DIR/bin/$name"
+   cd "$DIR/ngrok" && CGO_ENABLED=0 make release-client && mv bin/ngrok "$DIR/bin/$name/ngrok"
+   cd "$DIR/ngrok" && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 make release-client && mv bin/linux_amd64 "$DIR/bin/$name/"
+   cd "$DIR/ngrok" && CGO_ENABLED=0 GOOS=windows GOARCH=amd64 make release-client && mv bin/windows_amd64 "$DIR/bin/$name/"
+   cd "$DIR"
+done
